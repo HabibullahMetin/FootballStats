40217bad874c7fa9ccaac1c7514b0ead93d3096b
HabibullahMetin <53422805+HabibullahMetin@users.noreply.github.com>
2019-12-21 22:45:01 +0300

    Update Results.js

--- screens/Results.js
+++ screens/Results.js
@@ -41,14 +41,41 @@ export default class Results extends React.Component{
         blurOnSubmit={false}
       />
       <Text  style = {{fontSize : 15, color : "red"}}> Günler arası fark en fazla 7 olmalıdır. </Text>
+      <Text  style = {{fontSize : 15, color : "black"}}> Ligi seçiniz </Text>
 
-    <TextInput style= {{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        underlineColorAndroid="transparent"
-        placeholder = " Ligi giriniz"
-        placeholderTextColor ="black"
-        onChangeText= {league => this.setState({ league })}
-        blurOnSubmit={false}
-      />
+      <Picker
+       selectedValue={this.state.league}
+       style={{height: 25, width: 50}}
+       onValueChange={(itemValue, itemIndex) =>
+        this.setState({league: itemValue})
+     }>
+  <Picker.Item label="US Major League Soccer" value="US Major League Soccer" />
+  <Picker.Item label="Dutch Eredivisie" value="Dutch Eredivisie" />
+  <Picker.Item label="Internationals" value="Internationals" />
+  <Picker.Item label="Italian Serie A" value="Italian Serie A" />
+  <Picker.Item label="Spanish La Liga" value="Spanish La Liga" />
+  <Picker.Item label="Champions League" value="Champions League" />
+  <Picker.Item label="European Championship Finals" value="European Championship Finals" />
+  <Picker.Item label="English Premier League" value="English Premier League" />
+  <Picker.Item label="European World Cup Qualifiers" value="European World Cup Qualifiers" />
+  <Picker.Item label="Copa America" value="Copa America" />
+  <Picker.Item label="Brazilian Serie A" value="Brazilian Serie A" />
+  <Picker.Item label="African World Cup Qualifiers" value="African World Cup Qualifiers" />
+  <Picker.Item label="World Cup" value="World Cup" />
+  <Picker.Item label="French Ligue 1" value="French Ligue 1" />
+  <Picker.Item label="European Championship Qualifiers" value="European Championship Qualifiers" />
+  <Picker.Item label="Argentina Superliga" value="Argentina Superliga" />
+  <Picker.Item label="Champions League Qualifying" value="Champions League Qualifying" />
+  <Picker.Item label="German Bundesliga" value="German Bundesliga" />
+  <Picker.Item label="Belgian Jupiler Pro League" value="Belgian Jupiler Pro League" />
+  <Picker.Item label="Portuguese Primeira Liga" value="Portuguese Primeira Liga" />
+  <Picker.Item label="UEFA Europa League" value="UEFA Europa League" />
+  <Picker.Item label="FIFA Club World Cup" value="FIFA Club World Cup" />
+  <Picker.Item label="Copa Libertadores" value="Copa Libertadores" />
+  <Picker.Item label="Confederations Cup" value="Confederations Cup" />
+  <Picker.Item label="Concacaf League" value="Concacaf League" />
+  <Picker.Item label="UEFA Nations League" value="UEFA Nations League" />
+</Picker>
         <Button
           title="Sonuçları Getir"
           color = "dodgerblue"
