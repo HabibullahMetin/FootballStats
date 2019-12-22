8c8b0b55132cb465969b8aec22053489d1037566
HabibullahMetin <53422805+HabibullahMetin@users.noreply.github.com>
2019-12-22 21:29:23 +0300

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -42,12 +42,3 @@ export const fetchCalendars = async (startDate ,endDate ,leagueName) => {
   return request;
 };
 
-
-// export const fetchWorldCupResults = async() => {
-//   const response = await fetch("https://stroccoli-futbol-science-v1.p.rapidapi.com/world_cup/results/2014", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "stroccoli-futbol-science-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "4ce371a3a4msh9553c63153a21f5p1ccfbcjsn60f8b10805e7"
-// 	}}).then(response => response.json()).then(data => {console.log(data)}).catch(err => {console.log(err)});
-// };
